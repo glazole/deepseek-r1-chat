c622391aa36e5dfcb19a74e7c0cb7f73db8e279e
Oleg Glazkov <glazkov-oleg@yandex.ru>
2025-02-05 19:27:06 +0300

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 # Запускаем Ollama в фоне
 ollama serve &
 # Даем ему время на запуск
-sleep 15
+sleep 35
 # Загружаем модели при старте (если они не загружены)
 ollama list | grep "deepseek-r1:1.5b" || ollama pull deepseek-r1:1.5b
 # ollama list | grep "deepseek-r1:14b" || ollama pull deepseek-r1:14b
