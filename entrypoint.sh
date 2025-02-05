13b73906cade693fb342f6c002f5240358a07311
Oleg Glazkov <glazkov-oleg@yandex.ru>
2025-02-05 13:55:50 +0300

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,8 @@
 ollama serve &
 # Даем ему время на запуск
 sleep 5
+# Загружаем модели при старте (если они не загружены)
+ollama list | grep "deepseek-r1:1.5b" || ollama pull deepseek-r1:1.5b
+ollama list | grep "deepseek-r1:3b" || ollama pull deepseek-r1:3b
 # Запускаем Gradio
 exec python app.py
